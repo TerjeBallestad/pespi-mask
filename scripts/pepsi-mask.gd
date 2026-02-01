bcc7ff002189cf162afbe5beb548ceaaa7db50de
Kryxzael <kasper.ramskjell98@gmail.com>
2026-02-01 16:19:51 +0100

    Add pepsi mask

---
+++ scripts/pepsi-mask.gd
@@ -0,0 +1,9 @@
+class_name PepsiMask extends ClickableObject
+
+func _ready() -> void:
+	super._ready()
+	object_interacted_with.connect(_on_interacted)
+
+func _on_interacted(_name: String):
+	GameState.put_on_mask()
+	queue_free()
