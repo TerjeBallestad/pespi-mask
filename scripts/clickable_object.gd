16e4b82d384d17027194bd8665fa0647fd5b558a
Kryxzael <kasper.ramskjell98@gmail.com>
2026-02-01 10:27:53 +0100

    Auto-connect signals in ClickableObject

--- scripts/clickable_object.gd
+++ scripts/clickable_object.gd
@@ -2,6 +2,10 @@ class_name ClickableObject extends Area2D
 
 signal object_interacted_with(name:String)
 
+func _ready():
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
 func get_clickable_manager():
 	return get_tree().root.get_child(0).find_child("ClickManager",false)
 
