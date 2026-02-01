79bea2480e99d01aa11bf8d4875e3f89a47c6a12
TerjeBallestad <terj.ba@gmail.com>
2026-02-01 11:52:19 +0100

    dialog portrait

--- scripts/dialog/dialog_box.gd
+++ scripts/dialog/dialog_box.gd
@@ -12,7 +12,10 @@ var slide_animation_duration := 1.0
 @export
 var speaker_label: RichTextLabel
 
-@export 
+@export
+var portrait_rect: TextureRect
+
+@export
 var text_label: CrawlingText
 
 @export
@@ -23,32 +26,32 @@ func _ready():
 	%DialogManager.entering_dialog.connect(_on_entering_dialog)
 	%DialogManager.exiting_dialog.connect(_on_exiting_dialog)
 	%DialogManager.exiting_dialog_playback.connect(_on_exiting_dialog_playback)
-	
+
 	for i in len(options_buttons):
 		options_buttons[i].button_up.connect(func():
 			choice_made.emit(self, %DialogManager, i)
 		)
-	
+
 	position.y = _original_y + size.y
 
 func _process(delta: float):
 	if %DialogManager.playing:
 		%DialogManager.current_dialog.process(delta, %DialogManager)
-		
+
 func _on_entering_dialog_playback():
 	slide_in()
-		
+
 func _on_entering_dialog(dialog: Dialog):
 	dialog.on_display(self, %DialogManager)
-	
+
 func _on_exiting_dialog(dialog: Dialog):
 	dialog.on_hide(self, %DialogManager)
-		
+
 func _on_exiting_dialog_playback():
 	slide_out()
 
 func slide_in():
 	create_tween().tween_property(self, "position:y", _original_y, slide_animation_duration)
-	
+
 func slide_out():
 	create_tween().tween_property(self, "position:y", _original_y + size.y, slide_animation_duration)
